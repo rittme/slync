9065f67e670821dbf4e833c22d8bd125cf8393c3
Bernardo Rittmeyer <bernardo@rittme.com>
2016-07-13 02:01:37 -0400

    ES6 styling

--- app/components/Bookmark/Bookmark.js
+++ app/components/Bookmark/Bookmark.js
@@ -1,62 +1,55 @@
-const React = require("react");
-const moment = require("moment");
-const getBestImage = require("lib/getBestImage");
-const classNames = require("classnames");
-const SiteIcon = require("components/SiteIcon/SiteIcon");
+import React, { PropTypes } from 'react';
+import moment from 'moment';
+import getBestImage from 'lib/getBestImage';
+import classNames from 'classnames';
+import SiteIcon from 'components/SiteIcon/SiteIcon';
 
-const Bookmark = React.createClass({
-  getInitialState() {
-    return {
-
-    };
-  },
-  render() {
-    const site = this.props;
-    const title = site.bookmarkTitle;
-    const image = getBestImage(site.images);
-    const imageUrl = image ? image.url : null;
-    const description = site.description || site.url;
-    const isPortrait = image ? image.height > image.width : false;
-    let contextMessage;
-    if (site.context_message) {
-      contextMessage = site.context_message;
-    } else if (site.bookmarkDateCreated) {
-      contextMessage = `Bookmarked ${moment(site.bookmarkDateCreated).fromNow()}`;
-    } else if (site.lastVisitDate) {
-      contextMessage = `Visited ${moment(site.lastVisitDate).fromNow()}`;
-    } else if (site.type === "bookmark") {
-      contextMessage = "Bookmarked recently";
-    } else {
-      contextMessage = "Visited recently";
-    }
-
-    const style = {};
-
-    if (imageUrl) {
-      style.backgroundImage = `url(${imageUrl})`;
-    } else {
-      style.backgroundColor = "#ccc";
-    }
-
-    return (<li className="bookmark-item">
-      <a onClick={this.props.onClick} href={site.url} ref="link">
-        <div className={classNames("bookmark-image", {portrait: isPortrait})} style={style} ref="image">
-          <SiteIcon className="bookmark-icon" height={40} width={40} site={site} ref="icon" showBackground={true} border={false} faviconSize={32} />
-        </div>
-        <div className="bookmark-details">
-          <div className="bookmark-info">
-            <div className="bookmark-text">
-              <h4 ref="title" className="bookmark-title">{title}</h4>
-              <p className="bookmark-description" ref="description">{description}</p>
-            </div>
-            <div className="bookmark-context" ref="contextMessage">{contextMessage}</div>
-          </div>
-        </div>
-        <div className="inner-border" />
-      </a>
-    </li>);
+const Bookmark = (site) => {
+  const title = site.bookmarkTitle;
+  const image = getBestImage(site.images);
+  const imageUrl = image ? image.url : null;
+  const description = site.description || site.url;
+  const isPortrait = image ? image.height > image.width : false;
+  let contextMessage;
+  if (site.context_message) {
+    contextMessage = site.context_message;
+  } else if (site.bookmarkDateCreated) {
+    contextMessage = `Bookmarked ${moment(site.bookmarkDateCreated).fromNow()}`;
+  } else if (site.lastVisitDate) {
+    contextMessage = `Visited ${moment(site.lastVisitDate).fromNow()}`;
+  } else if (site.type === "bookmark") {
+    contextMessage = "Bookmarked recently";
+  } else {
+    contextMessage = "Visited recently";
   }
-});
+
+  const style = {};
+
+  if (imageUrl) {
+    style.backgroundImage = `url(${imageUrl})`;
+  } else {
+    style.backgroundColor = "#ccc";
+  }
+
+  return (<li className="bookmark-item">
+    <a href={site.url} ref="link">
+      <div className={classNames("bookmark-image", {portrait: isPortrait})} style={style} ref="image">
+        <SiteIcon className="bookmark-icon" height={40} width={40} site={site} ref="icon" showBackground={true} border={false} faviconSize={32} />
+      </div>
+      <div className="bookmark-details">
+        <div className="bookmark-info">
+          <div className="bookmark-text">
+            <h4 ref="title" className="bookmark-title">{title}</h4>
+            <p className="bookmark-description" ref="description">{description}</p>
+          </div>
+          <div className="bookmark-context" ref="contextMessage">{contextMessage}</div>
+        </div>
+      </div>
+      <div className="inner-border" />
+    </a>
+  </li>);
+};
+
 Bookmark.propTypes = {
   accessTime: React.PropTypes.number,
   bookmarkDateCreated: React.PropTypes.number,
@@ -73,4 +66,5 @@ Bookmark.propTypes = {
   refreshTime: React.PropTypes.number,
   url: React.PropTypes.string
 };
-module.exports.Bookmark = Bookmark;
+
+export default Bookmark;
--- app/main.js
+++ app/main.js
@@ -1,7 +1,8 @@
-const React = require("react");
-const ReactDOM = require("react-dom");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BookmarksPage from "components/BookmarksPage/BookmarksPage"
 
-const {BookmarksPage} = require("components/BookmarksPage/BookmarksPage");
 const sites = require("lib/shim.json").RECENT_BOOKMARKS_RESPONSE;
 
+console.log(sites);
 ReactDOM.render(<BookmarksPage sites={sites} />, document.getElementById("root"));
